fbdbd84a977dda2405faa66340512fbd9de01560
Nanci-Guzman <guz.zulema23@gmail.com>
2024-12-05 18:52:24 -0300

    agregamos un titulo y un parrafo en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 function App() {
-
-
-  return ()
-<div></div>
+  return (
+    <section>
+      <h1>Hola mundo</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, sunt?
+      </p>
+    </section>
+  );
 }
 
-    
-
-export default App
+export default App;
